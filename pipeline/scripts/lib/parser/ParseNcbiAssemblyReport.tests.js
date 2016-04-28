36f3ef112cceedf2f52e5d26d1a4977c69aa5e36
Davi Ortega <daviortega@users.noreply.github.com>
2016-04-28 11:45:15 -0700

    First unit test for complete genomes.

--- pipeline/scripts/lib/parser/ParseNcbiAssemblyReport.tests.js
+++ pipeline/scripts/lib/parser/ParseNcbiAssemblyReport.tests.js
@@ -6,12 +6,32 @@ let fs = require('fs'),
 let ParseNcbiAssemblyReport = require('./ParseNcbiAssemblyReport.js')
 
 
-let inputFile = path.resolve(__dirname, 'GCF_000006765.1_ASM676v1_assembly_report.txt'),
-	inStream = fs.createReadStream(inputFile),
-	parseNcbiAssemblyReport = new ParseNcbiAssemblyReport(),
-	results = []
+describe.only('ParseNcbiAssemblyReport', function() {
+	it('streaming parser of complete NCBI Assembly Reports', function(done){
+		let inputFile = path.resolve(__dirname, 'GCF_000006765.1_ASM676v1_assembly_report.txt'),
+			inStream = fs.createReadStream(inputFile),
+			parseNcbiAssemblyReport = new ParseNcbiAssemblyReport(),
+			results = []
 
-inStream.pipe(parseNcbiAssemblyReport)
-.on('data', (result) => {
-	results.push(result)
+		inStream.pipe(parseNcbiAssemblyReport)
+		.on('data', (info) => {
+			results.push(info)
+		})
+		.on('finish', () => {
+			expect(results).deep.equal([ 
+				{ 	
+					name: 'ANONYMOUS',
+    				role: 'assembled-molecule',
+    				assigned_molecule: 'na',
+    				type: 'Chromosome',
+    				genbank_accession: 'AE004091.2',
+    				genbank_refseq_relationship: '=',
+    				refseq_accession: 'NC_002516.2',
+    				unit: 'Primary Assembly',
+    				undefined: 'na' 
+    			} 
+    		])
+    	done()
+		})
+	})
 })
