b2b148e9b7d8d467930c7b249b6b10fe22908fa8
Luke Ulrich <lukeulrich@users.noreply.github.com>
2016-05-05 11:07:12 -0400

    Update: added LineStream Update: refactored HmmscanResultReaderStream

--- pipeline/scripts/lib/tools/hmmer3/HmmscanResultReaderStream.js
+++ pipeline/scripts/lib/tools/hmmer3/HmmscanResultReaderStream.js
@@ -1,65 +1,124 @@
 'use strict'
 
 // Core node libraries
-let Transform = require('stream').Transform,
-	StringDecoder = require('string_decoder').StringDecoder,
-	assert = require('assert')
+let assert = require('assert')
 
 // Local includes
-let HmmscanResult = require('./HmmscanResult')
-
-// Constants
-let kRecordSeparator = '\n\/\/\n'
+let LineStream = require('../../streams/LineStream')
 
 /**
- * HmmscanResultReaderStream parses raw HmmScan data and streams out objects
- * containing these results.
+ * HmmscanResultReaderStream parses the textual output from the HMMER3 hmmscan tool
+ * and streams out all the domain hits for each query sequence.
+ * 
+ * By extending from LineStream, the input is processed line by line in a streaming
+ * fashion for optimal performance. 
  */
 module.exports =
-class HmmscanResultReaderStream extends Transform {
+class HmmscanResultReaderStream extends LineStream {
 	constructor() {
-		super({objectMode: true})
-
-		this.buffer_ = ''
-		this.decoder_ = new StringDecoder('utf8')
+		super()
+		this.reset_()
 	}
-
-	_transform(chunk, encoding, done) {
-		this.buffer_ += this.decoder_.write(chunk)
-
-		let recordTo = this.buffer_.indexOf(kRecordSeparator)
-
-		while (recordTo != -1) {
-			let record = this.buffer_.substr(0, recordTo)
-
-			this.parseHmmscanResult_(record)
-			this.buffer_ = this.buffer_.substr(recordTo + kRecordSeparator.length, this.buffer_.length)
-			recordTo = this.buffer_.indexOf(kRecordSeparator)
+	
+	_transform(line, encoding, done) {
+		if (this.skipRemainingLines_) {
+			let lineIsRecordSeparator = line[0] === '/' && line[1] === '/'
+			if (lineIsRecordSeparator) {
+				this.sortDomainsByConditionalEvalue_()
+				this.checkResultAndPush_()
+				this.reset_()
+			}
+			return done()
 		}
+
+		if (!line || this.isComment_(line))
+			return done()
+
+		if (/^Query:\s/.test(line))
+			this.parseHeader_(line)
+		else if (line[0] === '>' && line[1] === '>')
+			this.parseDomainName_(line)
+		else if (/^\s+\d+\s+!/.test(line))
+			this.parseDomainHit_(line)
+		else if (/^Internal pipeline statistics/.test(line))
+			this.skipRemainingLines_ = true
+
 		done()
 	}
 
-	_flush(done) {
-		if (!this.buffer_.length)
-			return done()
-		else if(this.buffer_.substr(0,4) == '[ok]')
-			return done()
-
-		let record = '',
-			recordTo = this.buffer_.indexOf(kRecordSeparator)
+	// ----------------------------------------------------
+	// Private methods
+	checkResultAndPush_() {
+		if (!this.queryName_)
+			throw new Error('Missing sequence name')
 		
-		if (recordTo !== -1)
-			record = this.buffer_.substr(0, recordTo)
-			this.parseHmmscanResult_(record)
-		
-		this.buffer_ = ''
-		done()
+		if (!this.queryLength_)
+			throw new Error('Missing sequence length')
+			
+		this.push({
+			queryName: this.queryName_,
+			queryLength: this.queryLength_,
+			domains: this.domains_
+		})
 	}
-
-	parseHmmscanResult_(record) {
-		assert(/\S/.test(record), 'record must not be empty')
-
-		let hmmscanResult = new HmmscanResult(record)
-		return this.push(hmmscanResult)
+	
+	isComment_(line) {
+		return line[0] === '#'
+	}
+	
+	parseHeader_(line) {
+		let matches = /^Query:\s+(\S+)\s+\[L\=(\d+)\]/.exec(line)
+		if (!matches)
+			throw new Error('Error while parsing header from query line')
+			
+		this.queryName_ = matches[1]
+		this.queryLength_ = parseInt(matches[2])
+	}
+	
+	parseDomainName_(line) {
+		let matches = /^>>\s+(\S+)/.exec(line)
+		if (!matches)
+			throw new Error('Error while parsing domain name')
+			
+		this.currentDomainName_ = matches[1]
+	}
+	
+	parseDomainHit_(line) {
+		let dMatch = line.split(/\s+/)
+		assert(dMatch.length === 17, 'Expected 16 elements of data ' + line)
+	
+		this.domains_.push({
+			name: this.currentDomainName_,
+			score: parseFloat(dMatch[3]),
+			bias: parseFloat(dMatch[4]),
+			c_evalue: parseFloat(dMatch[5]),
+			i_evalue: parseFloat(dMatch[6]),
+			hmm_from: parseInt(dMatch[7]),
+			hmm_to: parseInt(dMatch[8]),
+			hmm_cov: dMatch[9],
+			ali_from: parseInt(dMatch[10]),
+			ali_to: parseInt(dMatch[11]),
+			ali_cov: dMatch[12],
+			env_from: parseInt(dMatch[13]),
+			env_to: parseInt(dMatch[14]),
+			env_cov: dMatch[15],
+			acc: parseFloat(dMatch[16])
+		})
+	}
+	
+	reset_() {
+		this.queryName_ = null
+		this.queryLength_ = null
+		this.currentDomainName_ = null
+		this.domains_ = []
+		this.skipRemainingLines_ = false
+	}
+	
+	sortDomainsByConditionalEvalue_() {
+		this.domains_.sort((a, b) => {
+			return a.c_evalue > b.c_evalue ? 1 : 
+				b.c_evalue > a.c_evalue ? -1 :
+				0;
+		})
 	}
 }
