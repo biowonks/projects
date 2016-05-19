0c3ca9e2e656f63bcf8c60b98818cd1c0c392c0a
Luke Ulrich <lukeulrich@users.noreply.github.com>
2016-05-19 10:35:10 -0400

    Infrastructure: added gitattributes file to specify all files end in LF Infrastructure: docker production sets user id Infrastructure: docker compose files now reference host volume in OS portable manner

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Reference: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 # Get the directory of this script
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
