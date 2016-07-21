6e55a23f0d3a67d0406c82aa86e3f2762206f1bf
Ogun Adebali <adebali@users.noreply.github.com>
2016-07-20 21:51:17 -0400

    updated genome update fields

--- pipeline/modules/per-genome/Taxonomy.js
+++ pipeline/modules/per-genome/Taxonomy.js
@@ -29,8 +29,26 @@ class TaxonomyTableModule extends PerGenomePipelineModule {
         return TaxonomyService.fetchTaxonomyAndUpdateTable(this.genome_.species.taxonomyId)
         .then((taxonomyObject) => {
             return this.genome_.update({
-                kingdom: taxonomyObject.kingdom
-                }, {fields: 'kingdom'})
+                superkingdom: taxonomyObject.superkingdom,
+                phylum: taxonomyObject.phylum,
+                class: taxonomyObject.class,
+                orderr: taxonomyObject.order,
+                family: taxonomyObject.family,
+                genus: taxonomyObject.genus,
+                species: taxonomyObject.species,
+                strain: taxonomyObject.strain
+                },
+                {
+                    fields: [
+                                'superkingdom',
+                                'phylum',
+                                'class',
+                                'orderr',
+                                'family',
+                                'genus',
+                                'species',
+                                'strain']
+                })
             .then(() => {
                 this.logger_.info('Updated taxonomy fields')
             })
