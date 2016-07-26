983ea321512af004263b27675c2777fa3b1362a5
Luke Ulrich <lukeulrich@users.noreply.github.com>
2016-07-26 15:06:46 -0400

    Added ability to stream STDIN to hmmscan

--- pipeline/lib/streams/hmmscan-stream.tests.js
+++ pipeline/lib/streams/hmmscan-stream.tests.js
@@ -1,20 +1,38 @@
 'use strict'
 
 // Core
-const path = require('path')
+const fs = require('fs'),
+	path = require('path')
 
 // Local
 const hmmscanStream = require('./hmmscan-stream')
 
 describe('streams', function() {
-	describe('hmmscan stream', function() {
-		it('predicts domains and outputs result', function(done) {
-			// eslint-disable-next-line no-mixed-requires
-			let hmmDatabaseFile = path.resolve(__dirname, 'test-data', 'test.hmm'),
-				fastaFile = path.resolve(__dirname, 'test-data', 'hmmscan-seqs.faa'),
-				numHmmsInPfam29 = 16295,
-				hmmscan = hmmscanStream(hmmDatabaseFile, fastaFile, numHmmsInPfam29),
-				expectedResults = require('./test-data/hmmscan-results'), // eslint-disable-line global-require
+	describe('hmmscan', function() {
+		// eslint-disable-next-line no-mixed-requires
+		let hmmDatabaseFile = path.resolve(__dirname, 'test-data', 'test.hmm'),
+			fastaFile = path.resolve(__dirname, 'test-data', 'hmmscan-seqs.faa'),
+			numHmmsInPfam29 = 16295,
+			expectedResults = require('./test-data/hmmscan-results') // eslint-disable-line global-require
+
+		it('(streaming input) predicts domains', function(done) {
+			let inStream = fs.createReadStream(fastaFile),
+				hmmscan = hmmscanStream(hmmDatabaseFile, numHmmsInPfam29),
+				results = []
+
+			inStream.pipe(hmmscan)
+			.on('error', done)
+			.on('data', (result) => {
+				results.push(result)
+			})
+			.on('finish', () => {
+				expect(results).deep.equal(expectedResults)
+				done()
+			})
+		})
+
+		it('(file) predicts domains', function(done) {
+			let hmmscan = hmmscanStream.file(hmmDatabaseFile, fastaFile, numHmmsInPfam29),
 				results = []
 
 			hmmscan.on('error', done)
