43f8e027ebfcd0db3f1bb42d48ef839c4a4cacd3
Luke Ulrich <lukeulrich@users.noreply.github.com>
2016-09-05 17:43:41 -0400

    update docker-compose

--- mist-api/config/database.js
+++ mist-api/config/database.js
@@ -1,36 +1,8 @@
 'use strict'
 
-// Core
-let path = require('path')
+// Local
+let loadDatabaseConfig = require('../src/node_modules/mist-lib/db/config')
 
 module.exports = function(rootPath, packageJSON) {
-	return {
-		enabled: true,
-
-		// NOTE: The DATABASE_URL environment variable if defined will override these values.
-		name: '',
-		user: '',
-		password: '',
-
-		sequelizeOptions: {
-			protocol: 'postgres',
-			dialect: 'postgres',
-			port: 5432,
-			host: 'localhost',
-			dialectOptions: {
-				application_name: packageJSON.name,
-				ssl: true
-			},
-			logging: false,
-			pool: {
-				maxConnections: 10,
-				maxIdleTime: 1000
-			}
-		},
-
-		migrations: {
-			path: path.resolve(rootPath, 'db', 'migrations'),
-			pattern: /^\d{4}_[\w-_]+\.sql$/
-		}
-	}
+	return loadDatabaseConfig(packageJSON)
 }
