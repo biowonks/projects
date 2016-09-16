92e5e6b8ea28c41b99daeacacafb6b2ddda16ddb
Luke Ulrich <lukeulrich@users.noreply.github.com>
2016-09-16 14:22:16 -0400

    fix: always output junit/test-results.xml

--- bin/merge-junit-reports.js
+++ bin/merge-junit-reports.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+/**
+ * Usage: merge-junit-reports.js [<test-results.xml> ...]
+ *
+ *  Merges junit xml reports into a single xml result file.
+ */
+
 /* eslint-disable no-console */
 'use strict'
 
@@ -9,18 +15,7 @@ const fs = require('fs')
 // Vendor
 const junitReportMerger = require('junit-report-merger')
 
-// Other
-const kUsage = `Usage: merge-junit-reports.js <test-results.xml> [<test-results.xml> ...]
-
-  Merges junit xml reports into a single xml result file.
-`
-
-let sourceFiles = process.argv.slice(2) // eslint-disable-line no-magic-numbers
-if (!sourceFiles.length) {
-	console.error(kUsage)
-	process.exit(1)
-}
-
-let sourceStreams = sourceFiles.map((sourceFile) => fs.createReadStream(sourceFile))
+let sourceFiles = process.argv.slice(2), // eslint-disable-line no-magic-numbers
+	sourceStreams = sourceFiles.map((sourceFile) => fs.createReadStream(sourceFile))
 
 junitReportMerger.mergeStreams(process.stdout, sourceStreams)
