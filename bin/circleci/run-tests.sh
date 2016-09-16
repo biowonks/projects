6ae057e3d2271f65bcbe4217bce2f34f3cf54185
Luke Ulrich <lukeulrich@users.noreply.github.com>
2016-09-16 12:27:19 -0400

    run coverage instead of test

--- bin/circleci/run-tests.sh
+++ bin/circleci/run-tests.sh
@@ -9,7 +9,7 @@ cd $ROOT
 
 for PROJECT in "$@"; do
 	echo "====> Running $PROJECT tests"
-	docker run -e CI=true -e MOCHA_REPORTER=mocha-circleci-reporter -v $ROOT:/app -w /app/$PROJECT biowonks/node-bootstrap npm test
+	docker run -e CI=true -e MOCHA_REPORTER=mocha-circleci-reporter -v $ROOT:/app -w /app/$PROJECT biowonks/node-bootstrap npm run coverage
 	#          ^^^^^^^^^^ inform scripts that we are a CI server
 
 	# Create a file to indicate that this project has been built
