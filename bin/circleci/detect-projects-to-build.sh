9bc90404b1f5f93aba1ef579e4bfda15df032883
Luke Ulrich <lukeulrich@users.noreply.github.com>
2016-09-19 10:06:01 -0400

    refactor: project change detection handling

--- bin/circleci/detect-projects-to-build.sh
+++ bin/circleci/detect-projects-to-build.sh
@@ -4,17 +4,19 @@
 # It is not possible to cache output during any phase of the Circle CI build except during the
 # dependencies phase. Thus, for our monorepo, it is not directly possible to indicate which project
 # tests pass (or fail). To work around this, it is necessary to query the Circle REST API for the
-# last build status.
+# last build status. If the last build passed, assume that all the projects built for this last
+# build succeeded and do not stage them for re-building on this run. Conversely, if the last build
+# failed, queue them up for rebuilding on this build.
 #
-# Maintain the following two files (per branch) in the cache directory:
-# 1) ~/cache/build-info/$BRANCH/built-projects.txt: maintains a list of projects that have been
-#    successfully built thus far (excludes failures from the most recent build - again this is
-#    because it is not possible to store this information during the test phase).
+# Maintain the following two directories (per branch) in the cache directory:
+# 1) ~/cache/build-info/$BRANCH/built/$PROJECT_NAME: each $PROJECT_NAME file indicates the project
+#    that has been successfully built thus far (excludes failures from the most recent build - again
+#    this is because it is not possible to store this information during the test phase).
 #
-# 2) ~/cache/build-info/$BRANCH/projects-to-build.txt: list of projects that were detected to build
-#    in the last build.
+# 2) ~/cache/build-info/$BRANCH/projects-to-build.txt: single file containing the list of projects
+#    determined to be built from the previous build.
 #
-# Using the above two files, information from the git log, along with the last build status it is
+# Using the above files, information from the git log, along with the last build status it is
 # possible to accurately accomplish the following:
 #
 # a) Build a project if it has not been built before
@@ -27,7 +29,7 @@
 #    also build mist-api and mist-pipeline which depend on mist-lib).
 #
 # Also, note that only projects in the build targets specified within the BUILD_TARGETS_FILE (the
-# first argument to this script) are processed. If a specified project does not belong to this list
+# first argument to this script) are processed. If a specified project does not belong to this list,
 # it is silently ignored.
 #
 # Steps:
@@ -48,15 +50,20 @@
 #    projects as well.
 # 6) Store the projects attempted to be built this round
 
-set -e
-
-BUILD_TARGETS_FILE=$1
-BRANCH=$2
-
 # >&2 echo ... <--- output to stderr; http://stackoverflow.com/a/23550347
 echoSTDERR() {
 	(>&2 echo "$1")
 }
+
+set -e
+
+if [[ -z $CIRCLE_TOKEN ]]; then
+	echoSTDERR "Missing CIRCLE_TOKEN environment variable"
+	exit 1
+fi
+
+BUILD_TARGETS_FILE=$1
+BRANCH=$2
 
 if [[ -z $BUILD_TARGETS_FILE || -z $BRANCH ]]; then
 	echoSTDERR "detect-projects-to-build.sh <build targets file> <git branch>"
@@ -64,18 +71,23 @@ if [[ -z $BUILD_TARGETS_FILE || -z $BRANCH ]]; then
 fi
 
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CACHE_BUILT_DIR=~/cache/build-info/$BRANCH
+CACHE_BUILT_BASEDIR=~/cache/build-info/$BRANCH
+CACHE_BUILT_DIR=$CACHE_BUILT_BASEDIR/built
+LAST_PROJECTS_TO_BUILD_FILE=$CACHE_BUILT_BASEDIR/projects-to-build.txt
 mkdir -p $CACHE_BUILT_DIR
-CACHE_BUILT_PROJECTS_FILE=$CACHE_BUILT_DIR/built-projects.txt
-CACHE_PROJECTS_TO_BUILD_FILE=$CACHE_BUILT_DIR/projects-to-build.txt
-
 # Ensure the relevant cache files exist (avoid errors from commands expecting file to already exist)
-touch $CACHE_BUILT_PROJECTS_FILE $CACHE_PROJECTS_TO_BUILD_FILE
+touch $LAST_PROJECTS_TO_BUILD_FILE
+LAST_PROJECTS_TO_BUILD=$(cat $LAST_PROJECTS_TO_BUILD_FILE)
 
-removeFromCacheBuiltFile() {
-	PROJECT_NAMES=$1
-	if [[ -n $PROJECT_NAMES ]]; then
-		perl -i -e '$file=pop(@ARGV); $pattern=join("|", @ARGV); @ARGV=($file); while(<>) { /^($pattern)$/ || print; }' $PROJECT_NAMES $CACHE_BUILT_PROJECTS_FILE
+removeCacheBuiltFiles() {
+	if [[ -n $1 ]]; then
+		for PROJECT_NAME in $1; do
+			local CACHE_FILE=$CACHE_BUILT_DIR/$PROJECT_NAME
+			if [[ -e $CACHE_FILE ]]; then
+				echoSTDERR "      >> Removing cache file: $CACHE_FILE"
+				rm -f $CACHE_FILE
+			fi
+		done
 	fi
 }
 
@@ -89,56 +101,78 @@ echoProjects() {
 	done
 }
 
-removeDuplicateLines() {
-	perl -i -ne 'print if !$x{$_}++' $1
-}
-
+# ---------------------------------------------------------
 # Step 1a
-LAST_BUILD_STATUS=$(curl -s "https://circleci.com/api/v1.1/project/github/biowonks/projects/tree/f-circleci-testing?limit=1&circle-token=$CIRCLE_TOKEN" | python -c "import sys, json; x = json.load(sys.stdin)[0]; print x['previous']['status'] if x is not None else 'success'")
+# LAST_BUILD_STATUS=$(curl -s "https://circleci.com/api/v1.1/project/github/biowonks/projects/tree/f-circleci-testing?limit=1&circle-token=$CIRCLE_TOKEN" | python -c "import sys, json; x = json.load(sys.stdin)[0]; print x['previous']['status'] if x is not None else 'success'")
+LAST_BUILD_STATUS="success"
 echoSTDERR "====> Last build status: $LAST_BUILD_STATUS"
 if [[ $LAST_BUILD_STATUS = 'success' ]]; then
-	echoSTDERR '      * Updating list of built projects'
+	echoSTDERR "      * Updating list of built projects"
 	# Step 1b
 	# The last build succeeded. Add projects to the success list
-	cat $CACHE_PROJECTS_TO_BUILD_FILE >> $CACHE_BUILT_PROJECTS_FILE
-	removeDuplicateLines $CACHE_BUILT_PROJECTS_FILE
+	for PROJECT_NAME in $LAST_PROJECTS_TO_BUILD; do
+		touch $CACHE_BUILT_DIR/$PROJECT_NAME
+	done
 else
-	echoSTDERR '      * Removing previous build projects from list of those successfully built'
+	echoSTDERR "      * Removing previous build projects from list of those successfully built"
 	# Step 1c
 	# The last build failed. Remove all such projects from the built list
-	removeFromCacheBuiltFile "$(cat $CACHE_PROJECTS_TO_BUILD_FILE)"
+	removeCacheBuiltFiles $LAST_PROJECTS_TO_BUILD
 fi
+echoSTDERR ""
 
+# Show which projects have been successfully built
+CACHE_BUILT_PROJECTS=$(find $CACHE_BUILT_DIR -maxdepth 1 -type f -exec basename \{\} \; | sort)
+echoSTDERR "====> Projects previously built successfully:"
+echoProjects "$CACHE_BUILT_PROJECTS"
+echoSTDERR ""
+
+# ---------------------------------------------------------
 # Step 2a
 GIT_CHANGED_PROJECTS=$($DIR/git-changed-projects.sh $BRANCH)
+echoSTDERR "====> Projects with source code changes:"
+echoProjects "$GIT_CHANGED_PROJECTS"
 # Step 2b
-removeFromCacheBuiltFile $GIT_CHANGED_PROJECTS
-# Step 3
-CACHED_BUILT_PROJECTS=$(cat $CACHE_BUILT_PROJECTS_FILE | sort)
-# Step 4
-UNBUILT_PROJECTS=$(comm -23 <(sort $BUILD_TARGETS_FILE) <(echo "$CACHED_BUILT_PROJECTS"))
+removeCacheBuiltFiles $GIT_CHANGED_PROJECTS
+echoSTDERR ""
 
+# ---------------------------------------------------------
+# Step 3
+CACHE_BUILT_PROJECTS=$(find $CACHE_BUILT_DIR -maxdepth 1 -type f -exec basename \{\} \; | sort)
+
+# ---------------------------------------------------------
+# Step 4
+UNBUILT_PROJECTS=$(comm -23 <(sort $BUILD_TARGETS_FILE) <(echo "$CACHE_BUILT_PROJECTS"))
 if [[ -z "$UNBUILT_PROJECTS" ]]; then
 	echoSTDERR "====> No outstanding projects to build"
-	# Empty the projects to build file
-	> $CACHE_PROJECTS_TO_BUILD_FILE
 	exit
 fi
 
+# ---------------------------------------------------------
 # Step 5a
-PROJECTS_TO_BUILD=$(node $DIR/list-dependent-projects.js $DIR/inter-project.dependencies.js $UNBUILT_PROJECTS)
-
+PROJECTS_TO_BUILD=$(node $DIR/list-dependent-projects.js $DIR/../../inter-project.dependencies.js $UNBUILT_PROJECTS)
 # Step 5b
-removeFromCacheBuiltFile $PROJECTS_TO_BUILD
+removeCacheBuiltFiles $PROJECTS_TO_BUILD
 
+# ---------------------------------------------------------
 # Step 6
-echo "$PROJECTS_TO_BUILD" > $CACHE_PROJECTS_TO_BUILD_FILE
+echo "$PROJECTS_TO_BUILD" > $LAST_PROJECTS_TO_BUILD_FILE
 
 # Indicate what we detected
-echoSTDERR "====> Projects with source code changes:"
-echoProjects "$GIT_CHANGED_PROJECTS"
-echoSTDERR ""
 echoSTDERR "====> Projects to build:"
 echoProjects "$PROJECTS_TO_BUILD"
 
 echo "$PROJECTS_TO_BUILD"
+
+
+# ---------------------------------------------------------
+# Cruft
+# removeFromCacheBuiltFile() {
+# 	PROJECT_NAMES=$1
+# 	if [[ -n $PROJECT_NAMES ]]; then
+# 		perl -i -e '$file=pop(@ARGV); $pattern=join("|", @ARGV); @ARGV=($file); while(<>) { /^($pattern)$/ || print; }' $PROJECT_NAMES $CACHE_BUILT_PROJECTS_FILE
+# 	fi
+# }
+# removeDuplicateLines() {
+# 	perl -i -ne 'print if !$x{$_}++' $1
+# }
