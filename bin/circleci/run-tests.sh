e42e869c02e2e34523fd4d4a3fc2be8bae125b86
Luke Ulrich <lukeulrich@users.noreply.github.com>
2016-09-19 11:13:30 -0400

    save code coverage to artifacts directory

--- bin/circleci/run-tests.sh
+++ bin/circleci/run-tests.sh
@@ -11,4 +11,8 @@ for PROJECT in "$@"; do
 	echo "====> Running $PROJECT tests"
 	docker run -e CI=true -e MOCHA_REPORTER=mocha-circleci-reporter -v $ROOT:/app -w /app/$PROJECT biowonks/node-bootstrap npm run coverage
 	#          ^^^^^^^^^^ inform scripts that we are a CI server
+
+	echo "      - Copying coverage report to artifacts"
+	mkdir -p $CIRCLE_ARTIFACTS/coverage/$PROJECT
+	cp -r $ROOT/$PROJECT/testing/coverage/lcov-report $CIRCLE_ARTIFACTS/coverage/$PROJECT
 done
