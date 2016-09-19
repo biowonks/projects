a8e255ef75738f222e94d788eede36fe6b5e9181
Luke Ulrich <lukeulrich@users.noreply.github.com>
2016-09-19 16:48:45 -0400

    added logic to login to heroku

---
+++ bin/circleci/setup-heroku.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [[ -z $CIRCLECI ]]; then
+	echo "This script is intended to be performed in the CircleCI environment only"
+	exit 1
+fi
+
+if [[ -z $HEROKU_API_TOKEN ]]; then
+	echo "HEROKU_API_TOKEN is not set"
+	exit 1
+fi
+
+if [[ -z $HEROKU_USER ]]; then
+	echo "HEROKU_USER is not set"
+	exit 1
+fi
+
+if [ ! -f "${HOME}/.ssh/id_heroku.com.pub" ]; then
+  echo "SSH is not set for Heroku in CircleCI. set here https://circleci.com/gh/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/edit#ssh"
+  exit 1
+fi
+
+cat > $HOME/.netrc <<EOF
+machine api.heroku.com
+	login $HEROKU_USER
+	password $HEROKU_API_TOKEN
+EOF
+
+chmod 600 ~/.netrc
+
+echo "====> Logging into Heroku"
+heroku login
