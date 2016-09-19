06e60d1669c14967c504e01b91611f701fb2d03b
Luke Ulrich <lukeulrich@users.noreply.github.com>
2016-09-19 18:08:04 -0400

    fix copying coverage reports to CIRCLE_ARTIFACTS

--- bin/circleci/run-tests.sh
+++ bin/circleci/run-tests.sh
@@ -7,14 +7,13 @@ ROOT=$DIR/../..
 
 cd $ROOT
 
-mkdir -p $CIRCLE_ARTIFACTS/coverage
-
 for PROJECT in "$@"; do
 	echo "====> Running $PROJECT tests"
 	docker run -e CI=true -e MOCHA_REPORTER=mocha-circleci-reporter -v $ROOT:/app -w /app/$PROJECT biowonks/node-bootstrap npm run coverage
 	#          ^^^^^^^^^^ inform scripts that we are a CI server
 
 	echo "      - Copying coverage report to artifacts"
-	cp -r $ROOT/$PROJECT/testing/coverage/lcov-report $CIRCLE_ARTIFACTS/coverage/$PROJECT
+	mkdir -p $CIRCLE_ARTIFACTS/coverage/$PROJECT
+	cp -r $ROOT/$PROJECT/testing/coverage/lcov-report/* $CIRCLE_ARTIFACTS/coverage/$PROJECT
 	cp $ROOT/$PROJECT/testing/coverage/* $CIRCLE_ARTIFACTS/coverage/$PROJECT 2>/dev/null
 done
