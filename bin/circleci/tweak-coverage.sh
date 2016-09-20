41a1ee0c440b8ee7ba8de427de107776519d97c9
Luke Ulrich <lukeulrich@users.noreply.github.com>
2016-09-20 10:19:10 -0400

    more ci polish

--- bin/circleci/tweak-coverage.sh
+++ bin/circleci/tweak-coverage.sh
@@ -2,26 +2,26 @@
 
 set -e
 
-BRANCH=$1
-shift
-
-if [[ -z $BRANCH || -z $1 ]]; then
+if [[ $# -lt 2 ]]; then
 	echo "Usage: tweak-coverage.sh <branch> <build target> ..."
 	exit 1
 fi
+
+BRANCH=$1
+shift
 
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT=$DIR/../..
 
 cd $ROOT
 
-# Retrieve any prior code coverage reports from S3
+# Retrieve any prior code coverage reports from s3
 echo "====> Creating temporary directories: /tmp/s3 and /tmp/coverage"
 mkdir -p /tmp/s3 /tmp/coverage
 echo ""
 
-echo "====> Downloading previous coverage files from S3"
-aws s3api get-object --bucket biowonks --key circleci/$BRANCH/coverage.tar.gz /tmp/s3/coverage.tar.gz 2>/dev/null || true
+echo "====> Downloading previous coverage files from s3"
+aws s3 cp "s3://biowonks/circleci/$BRANCH/coverage.tar.gz" /tmp/s3/coverage.tar.gz 2>/dev/null || true
 if [[ -s /tmp/s3/coverage.tar.gz ]]; then
 	tar -C /tmp/s3 -xf /tmp/s3/coverage.tar.gz
 else
@@ -45,17 +45,17 @@ echo ""
 
 # Create the new tarball coverage
 echo "====> Saving results into new tarball"
-tar -C /tmp -czf coverage.tar.gz coverage
-aws s3 cp --storage-class=REDUCED_REDUNDANCY coverage.tar.gz s3://biowonks/circleci/$BRANCH/coverage.tar.gz
+tar -C /tmp -czf /tmp/coverage.tar.gz coverage
+aws s3 cp --storage-class=REDUCED_REDUNDANCY /tmp/coverage.tar.gz "s3://biowonks/circleci/$BRANCH/coverage.tar.gz"
 echo ""
 
 # Create the final combined report
 echo "====> Combining code coverage reports"
 mkdir -p $CIRCLE_ARTIFACTS/coverage # In case it does not already exist
-node_modules/.bin/istanbul report --root /tmp/coverage --dir /tmp/coverage/all json lcov 2>/dev/null
+node_modules/.bin/istanbul report --root /tmp/coverage --dir /tmp/coverage/all json lcov
 cp -r /tmp/coverage/all/lcov-report $CIRCLE_ARTIFACTS/coverage/all
 mv /tmp/coverage/all/coverage-final.json /tmp/coverage/all/coverage.json
-cp /tmp/coverage/all/* $CIRCLE_ARTIFACTS/coverage/all > /dev/null || true
+cp /tmp/coverage/all/* $CIRCLE_ARTIFACTS/coverage/all 2> /dev/null || true
 echo ""
 
 echo "====> Uploading results to codecov"
