bbd7942a936f398d949b7948fe12b7a671485443
Luke Ulrich <lukeulrich@users.noreply.github.com>
2016-10-07 15:50:51 -0400

    fix: install dependencies for mist-api docs on circleci (not docker)

--- bin/circleci/util/install-dependencies.sh
+++ bin/circleci/util/install-dependencies.sh
@@ -5,15 +5,15 @@ set -e
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT=$DIR/../../..
 
-cd $ROOT
-
 for PROJECT in "$@"; do
 	echo "====> Installing $PROJECT dependencies"
 	docker run -e CI=true -v $ROOT:/app -w /app/$PROJECT biowonks/node-bootstrap /bin/bash -c 'npm prune && npm install'
 	#          ^^^^^^^^^^ inform scripts that we are a CI server
 
 	if [[ $PROJECT = 'mist-api' ]]; then
-		echo "====> Installing $PROJECT docs dependencies"
-		docker run -e CI=true -v $ROOT:/app -w /app/$PROJECT/src/docs biowonks/node-bootstrap /bin/bash -c 'npm prune && npm install'
+		echo "====> Installing $PROJECT docs dependencies (not on docker)"
+		pushd $ROOT/$PROJECT/src/docs
+		npm prune && npm install
+		popd
 	fi
 done
