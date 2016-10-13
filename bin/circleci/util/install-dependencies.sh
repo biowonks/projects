69ab9cbd2f3689eb7abb9b7d0c088df5cc716a21
Luke Ulrich <lukeulrich@users.github.noreply.com>
2016-10-13 18:27:00 -0400

    no longer compile mist-api doc dependencies outside of docker

--- bin/circleci/util/install-dependencies.sh
+++ bin/circleci/util/install-dependencies.sh
@@ -9,11 +9,4 @@ for PROJECT in "$@"; do
 	echo "====> Installing $PROJECT dependencies"
 	docker run -e CI=true -v $ROOT:/app -w /app/$PROJECT biowonks/node-bootstrap /bin/bash -c 'npm prune && npm install'
 	#          ^^^^^^^^^^ inform scripts that we are a CI server
-
-	if [[ $PROJECT = 'mist-api' ]]; then
-		echo "====> Installing $PROJECT docs dependencies (not on docker)"
-		pushd $ROOT/$PROJECT/src/docs
-		npm prune && npm install
-		popd
-	fi
 done
