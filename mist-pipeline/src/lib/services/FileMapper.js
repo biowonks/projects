ab859d4f29a1b2a024a5193a99bac21906ec9475
Luke Ulrich <lukeulrich@users.github.noreply.com>
2016-10-21 11:26:59 -0400

    replace # signs with underscores

--- mist-pipeline/src/lib/services/FileMapper.js
+++ mist-pipeline/src/lib/services/FileMapper.js
@@ -46,6 +46,7 @@ class FileMapper {
 			throw new Error('Genome has not been set. Please call setGenome first')
 
 		let urlAssemblyName = this.genome_.assembly_name.replace(/ /g, '_')
+			.replace(/#/g, '_')
 		return `${this.compoundAccession_()}_${urlAssemblyName}`
 	}
 
