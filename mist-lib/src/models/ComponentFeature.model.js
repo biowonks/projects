1c396fb59289c5a34d6ddad8c6034d7eae1a2fe3
Luke Ulrich <lukeulrich@users.github.noreply.com>
2016-10-26 16:47:53 -0400

    refactored Component, ComponentFeature, and Gene models

--- mist-lib/src/models/ComponentFeature.model.js
+++ mist-lib/src/models/ComponentFeature.model.js
@@ -4,6 +4,12 @@ module.exports = function(Sequelize, models, extras) {
 	let fields = {
 		component_id: extras.requiredPositiveInteger(),
 		gene_id: extras.positiveInteger(),
+		locus: {
+			type: Sequelize.TEXT,
+			validate: {
+				notEmpty: true
+			}
+		},
 		key: extras.requiredText(),
 		location: extras.requiredText(),
 		strand: extras.dnaStrand(),
