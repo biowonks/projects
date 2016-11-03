b44980db5e91d3c2ba7b3f98882a67715cf9d793
Luke Ulrich <lukeulrich@users.github.noreply.com>
2016-11-03 10:06:59 -0400

    use rsync when downloading genomic data

--- mist-pipeline/src/lib/services/FileMapper.js
+++ mist-pipeline/src/lib/services/FileMapper.js
@@ -24,21 +24,23 @@ class FileMapper {
 	 * @param {string} sourceType type of NCBI data file being analyzed
 	 * @returns {string} the relevant file name
 	 */
-	fileNameFor(sourceType) {
-		let prefix = this.ncbiPrefix()
-
+	localFileNameFor(sourceType) {
 		switch (sourceType) {
 			case 'checksums':				return 'md5checksums.txt'
-			case 'genomic-genbank':			return `${prefix}_genomic.gbff.gz`
-			case 'genomic-fasta':			return `${prefix}_genomic.fna.gz`
-			case 'protein-fasta':			return `${prefix}_protein.faa.gz`
-			case 'assembly-report':			return `${prefix}_assembly_report.txt`
-			case 'genes-gff':				return `${prefix}_genomic.gff.gz`
-			case 'feature-table':			return `${prefix}_feature_table.txt.gz`
+			case 'genomic-genbank':			return 'genomic.gbff.gz'
+			case 'genomic-fasta':			return 'genomic.fna.gz'
+			case 'protein-fasta':			return 'protein.faa.gz'
+			case 'assembly-report':			return 'assembly_report.txt'
+			case 'genes-gff':				return 'genomic.gff.gz'
+			case 'feature-table':			return 'feature_table.txt.gz'
 
 			default:
 				throw new Error(`${sourceType} is not supported`)
 		}
+	}
+
+	ncbiFileNameFor(sourceType) {
+		return this.ncbiPrefix() + '_' + this.localFileNameFor(sourceType)
 	}
 
 	ncbiPrefix() {
@@ -55,7 +57,15 @@ class FileMapper {
 	}
 
 	ncbiUrlFor(sourceType) {
-		return `${this.genome_.ftp_path}/${this.fileNameFor(sourceType)}`
+		return `${this.genome_.ftp_path}/${this.ncbiFileNameFor(sourceType)}`
+	}
+
+	ncbiRsyncUrlFor(sourceType) {
+		let url = `${this.genome_.ftp_path}/`.replace(/^ftp:/, 'rsync:')
+		if (sourceType !== 'checksums')
+			url += '*_'
+		url += this.localFileNameFor(sourceType)
+		return url
 	}
 
 	genomeRootPath() {
@@ -63,6 +73,6 @@ class FileMapper {
 	}
 
 	pathFor(sourceType) {
-		return path.resolve(this.genomeRootPath(), this.fileNameFor(sourceType))
+		return path.resolve(this.genomeRootPath(), this.localFileNameFor(sourceType))
 	}
 }
