1b0a7a67ab2d0fd140d663651139e21c007990d3
Luke Ulrich <lukeulrich@users.github.noreply.com>
2016-11-17 16:40:07 -0500

    added tmhmm2 aseq compute sub module

---
+++ mist-pipeline/src/lib/services/AseqsComputeService/tool-runners/Tmhmm2ToolRunner.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// Local
+const FastaStreamToolRunner = require('./FastaStreamToolRunner'),
+	tmhmm2Stream = require('lib/streams/tmhmm2-stream')
+
+module.exports =
+class Tmhmm2ToolRunner extends FastaStreamToolRunner {
+	handleResult_(aseq, result) {
+		aseq.tmhmm2 = {
+			topology: result.topology,
+			tms: result.tms
+		}
+	}
+
+	toolStream_() {
+		return tmhmm2Stream()
+	}
+}
+
+module.exports.meta = {
+	id: 'tmhmm2',
+	description: 'predict transmembrane regions'
+}
