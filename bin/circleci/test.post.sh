cfcc54e0944294573ffae51607656cc41305946e
Luke Ulrich <lukeulrich@users.github.noreply.com>
2016-11-21 16:28:11 -0500

    simplify build by running all tests regardless of which projects changed

--- bin/circleci/test.post.sh
+++ bin/circleci/test.post.sh
@@ -7,5 +7,5 @@ DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT=$DIR/../..
 
 mkdir -p $CIRCLE_TEST_REPORTS/junit
-$ROOT/bin/merge-junit-reports.js $(awk '{print $1 "/test-results.xml" }' ~/projects-to-build.txt) > $CIRCLE_TEST_REPORTS/junit/test-results.xml
+$ROOT/bin/merge-junit-reports.js $(awk '{print $1 "/test-results.xml" }' $ROOT/build-targets.txt) > $CIRCLE_TEST_REPORTS/junit/test-results.xml
 $DIR/util/tweak-coverage.sh $CIRCLE_BRANCH $(cat $ROOT/build-targets.txt)
