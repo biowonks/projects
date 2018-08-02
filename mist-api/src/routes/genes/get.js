e913427e28ba4c510255b82c0543e3ea99ed5e44
Vadim Gumerov <netuns@gmail.com>
2018-08-02 17:43:11 -0400

    Allow to search genes by version, locus, old_locus, stable_id, product

--- mist-api/src/routes/genes/get.js
+++ mist-api/src/routes/genes/get.js
@@ -1,17 +1,59 @@
 'use strict'
+// Vendor
+const _ = require('lodash')
+
+// Local
+const util = require('core-lib/util')
 
 module.exports = function(app, middlewares, routeMiddlewares) {
 	let models = app.get('models'),
 		helper = app.get('lib').RouteHelper.for(models.Gene)
 
+
+	const textFieldNames = [
+			'version',
+			'locus',
+			'old_locus',
+			'stable_id',
+			'product'
+	]
+
+	const processSearch = (queryValue, target) => {
+		if (!queryValue)
+			return
+		if (!target)
+			throw new Error('processSearch must have a defined target')
+		const terms = util.splitIntoTerms(queryValue).map((term) => `%${term}%`)
+		if (terms.length > 0) {
+			textFieldNames
+			.forEach((fieldName) => {
+				_.set(target, `criteria.where.$or.${fieldName}.$ilike.$any`, terms)
+			})
+		}
+	}
+
 	return [
 		middlewares.parseCriteriaForMany(models.Gene, {
 			accessibleModels: [
+				models.Genome,
 				models.Component,
 				models.Aseq,
 				models.Dseq
+			],
+			maxPage: null,
+			permittedOrderFields: '*',
+			permittedWhereFields: [
+				'id',
+				...textFieldNames,
 			]
 		}),
+		(req, res, next) => {
+			// Provide for searching against textFieldNames (see above)
+			if (Reflect.has(req.query, 'search'))
+				processSearch(req.query.search, res.locals)
+
+			next()
+		},
 		helper.findManyHandler()
 	]
 }
