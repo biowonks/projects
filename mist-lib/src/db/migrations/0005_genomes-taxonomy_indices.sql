17623bfbc62185634b361fbcae8f712da6244602
Vadim Gumerov <netuns@gmail.com>
2018-09-19 13:53:43 -0400

    Lowercase key words in migration file

--- mist-lib/src/db/migrations/0005_genomes-taxonomy_indices.sql
+++ mist-lib/src/db/migrations/0005_genomes-taxonomy_indices.sql
@@ -1,23 +1,23 @@
-CREATE INDEX trgm_idx_genomes_name ON genomes USING gin (name gin_trgm_ops);
-CREATE INDEX trgm_idx_genomes_superkingdom ON genomes USING gin (superkingdom gin_trgm_ops);
-CREATE INDEX trgm_idx_genomes_phylum ON genomes USING gin (phylum gin_trgm_ops);
-CREATE INDEX trgm_idx_genomes_class ON genomes USING gin (class gin_trgm_ops);
-CREATE INDEX trgm_idx_genomes_orderr ON genomes USING gin (orderr gin_trgm_ops);
-CREATE INDEX trgm_idx_genomes_family ON genomes USING gin (family gin_trgm_ops);
-CREATE INDEX trgm_idx_genomes_genus ON genomes USING gin (genus gin_trgm_ops);
-CREATE INDEX trgm_idx_genomes_assembly_level ON genomes USING gin (assembly_level gin_trgm_ops);
-CREATE INDEX trgm_idx_genes_product ON genes USING gin (product gin_trgm_ops);
-CREATE index ON genes(old_locus);
+create index trgm_idx_genomes_name on genomes using gin (name gin_trgm_ops);
+create index trgm_idx_genomes_superkingdom on genomes using gin (superkingdom gin_trgm_ops);
+create index trgm_idx_genomes_phylum on genomes using gin (phylum gin_trgm_ops);
+create index trgm_idx_genomes_class on genomes using gin (class gin_trgm_ops);
+create index trgm_idx_genomes_orderr on genomes using gin (orderr gin_trgm_ops);
+create index trgm_idx_genomes_family on genomes using gin (family gin_trgm_ops);
+create index trgm_idx_genomes_genus on genomes using gin (genus gin_trgm_ops);
+create index trgm_idx_genomes_assembly_level on genomes using gin (assembly_level gin_trgm_ops);
+create index trgm_idx_genes_product on genes using gin (product gin_trgm_ops);
+create index on genes(old_locus);
 
 
 -- -- MIGRATION DOWN SQL
--- DROP INDEX trgm_idx_genomes_name;
--- DROP INDEX trgm_idx_genomes_superkingdom;
--- DROP INDEX trgm_idx_genomes_phylum;
--- DROP INDEX trgm_idx_genomes_class;
--- DROP INDEX trgm_idx_genomes_orderr;
--- DROP INDEX trgm_idx_genomes_family;
--- DROP INDEX trgm_idx_genomes_genus;
--- DROP INDEX trgm_idx_genomes_assemblylevel;
--- DROP INDEX genes_old_locus_idx;
--- DROP INDEX trgm_idx_genes_product;
+-- drop index trgm_idx_genomes_name;
+-- drop index trgm_idx_genomes_superkingdom;
+-- drop index trgm_idx_genomes_phylum;
+-- drop index trgm_idx_genomes_class;
+-- drop index trgm_idx_genomes_orderr;
+-- drop index trgm_idx_genomes_family;
+-- drop index trgm_idx_genomes_genus;
+-- drop index trgm_idx_genomes_assemblylevel;
+-- drop index genes_old_locus_idx;
+-- drop index trgm_idx_genes_product;
